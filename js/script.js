363444e141b8d1e8a4d77f9b424089640cd0e209
vipin9717chauhan <9717vipinchauhan@gmail.com>
2024-05-17 16:09:46 +0530

    notepad all files upload on github

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function show_result()
+{
+	var user_code = document.getElementById("code-return").value;;
+	var second_box = document.getElementById('right-box');
+	second_box.innerHTML = user_code;
+	
+}
